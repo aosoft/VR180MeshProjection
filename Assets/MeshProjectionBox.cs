c702bcef70a200735865a1f5cf692865048fc110
aosoft <tan-y@aosoft.jp>
2018-05-16 22:40:15 +0900

    Added Mesh Projection Box Parser, Sample apps

---
+++ Assets/MeshProjectionBox.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+
+namespace MeshProjectionBoxParser
+{
+	public struct MeshBoxVertex
+	{
+		public int x_index_delta;
+		public int y_index_delta;
+		public int z_index_delta;
+		public int u_index_delta;
+		public int v_index_delta;
+	}
+
+	public enum MeshBoxIndexType : int
+	{
+		Triangles = 0,
+		TriangleStrip = 1,
+		TriangleFan = 2
+	}
+
+	public class MeshBoxVertexList
+	{
+		public int texture_id;
+		public MeshBoxIndexType index_type;
+		public int[] index_as_delta;
+	}
+
+	public class MeshBox
+	{
+		public float[] coordinates;
+		public MeshBoxVertex[] verticies;
+		public MeshBoxVertexList[] vertex_lists;
+	}
+
+	public class MeshProjectionBox
+	{
+		/// <summary>
+		/// Mesh Projection Box のバイナリをパースする。
+		/// </summary>
+		/// <param name="bytes">size, boxtype 込みのバイナリ</param>
+		/// <returns></returns>
+		public static MeshBox[] Parse(byte[] bytes)
+		{
+			using (var s = new MemoryStream(bytes, false))
+			{
+				var boxSize = ReadUInt32(s);
+				var boxHeader = ReadUInt32(s);
+				var boxVerFlag = ReadUInt32(s);
+
+				var ret = new MeshProjectionBox();
+
+				var crc = ReadUInt32(s);
+				var encoding_four_cc = ReadUInt32(s);
+
+				if (encoding_four_cc == 0x64666c38)
+				{
+					using (var s2 = new DeflateStream(s, CompressionMode.Decompress, true))
+					{
+						return ParseMeshBoxes(s2);
+					}
+				}
+				else
+				{
+					return ParseMeshBoxes(s);
+				}
+			}
+		}
+
+		private static MeshBox[] ParseMeshBoxes(Stream s)
+		{
+			var ret = new List<MeshBox>();
+
+			try
+			{
+				while (true)
+				{
+					var box = ParseMeshBox(s);
+					if (box == null)
+					{
+						break;
+					}
+					ret.Add(box);
+				}
+			}
+			catch
+			{
+			}
+
+			return ret.ToArray();
+		}
+
+		private static MeshBox ParseMeshBox(Stream s)
+		{
+			var ret = new MeshBox();
+
+			var boxSize = ReadUInt32(s);
+			var boxHeader = ReadUInt32(s);
+
+			var coordinate_count = ReadUInt32(s);
+			ret.coordinates = new float[coordinate_count];
+			for (uint i = 0; i < coordinate_count; i++)
+			{
+				ret.coordinates[i] = ReadSingle(s);
+			}
+
+			var ccsb = (int)Math.Ceiling(Math.Log(coordinate_count * 2, 2));
+
+			var vertex_count = ReadUInt32(s);
+			ret.verticies = new MeshBoxVertex[vertex_count];
+
+			{
+				var br = new BitStreamReader(s);
+				for (uint i = 0; i < vertex_count; i++)
+				{
+					ret.verticies[i] = new MeshBoxVertex()
+					{
+						x_index_delta = FromEncodedUInt(br.ReadUInt32(ccsb)),
+						y_index_delta = FromEncodedUInt(br.ReadUInt32(ccsb)),
+						z_index_delta = FromEncodedUInt(br.ReadUInt32(ccsb)),
+						u_index_delta = FromEncodedUInt(br.ReadUInt32(ccsb)),
+						v_index_delta = FromEncodedUInt(br.ReadUInt32(ccsb))
+					};
+				}
+			}
+
+			var vertex_list_count = ReadUInt32(s);
+			ret.vertex_lists = new MeshBoxVertexList[vertex_list_count];
+			var vcsb = (int)Math.Ceiling(Math.Log(vertex_count * 2, 2));
+
+			{
+				var br = new BitStreamReader(s);
+				for (uint i = 0; i < vertex_list_count; i++)
+				{
+					var vertex_list = new MeshBoxVertexList()
+					{
+						texture_id = s.ReadByte(),
+						index_type = (MeshBoxIndexType)s.ReadByte()
+					};
+
+					var index_count = ReadUInt32(s);
+					vertex_list.index_as_delta = new int[index_count];
+					for (uint n = 0; n < index_count; n++)
+					{
+						vertex_list.index_as_delta[n] = FromEncodedUInt(br.ReadUInt32(vcsb));
+					}
+
+					ret.vertex_lists[i] = vertex_list;
+				}
+			}
+
+
+			return ret;
+		}
+
+		private static UInt32 ReadUInt32(Stream s)
+		{
+			return (ReadByte(s) << 24) | (ReadByte(s) << 16) | (ReadByte(s) << 8) | ReadByte(s);
+		}
+
+		private static UInt32 ReadByte(Stream s)
+		{
+			var ret = s.ReadByte();
+			if (ret < 0)
+			{
+				throw new Exception();
+			}
+			return (UInt32)ret;
+		}
+
+		private static int FromEncodedUInt(uint value)
+		{
+			return ((value & 1) == 0) ? (int)(value >> 1) : -(int)(value >> 1) - 1;
+		}
+
+		private static float ReadSingle(Stream s)
+		{
+			var bytes = new byte[4];
+			if (s.Read(bytes, 0, bytes.Length) < bytes.Length)
+			{
+				throw new Exception();
+			}
+			bytes = bytes.Reverse().ToArray();
+			return BitConverter.ToSingle(bytes, 0);
+		}
+
+
+		class BitStreamReader
+		{
+			private Stream _stream;
+			private int _lastBits;
+			private int _currentByte;
+
+			public BitStreamReader(Stream s)
+			{
+				_stream = s;
+				_lastBits = 0;
+				_currentByte = 0;
+			}
+
+			public int ReadBit()
+			{
+				if (_lastBits == 0)
+				{
+					_currentByte = _stream.ReadByte() & 0xff;
+					_lastBits = 8;
+				}
+				var ret = (_currentByte >> 7) & 1;
+				_currentByte <<= 1;
+				_lastBits--;
+				return ret;
+			}
+
+			public uint ReadUInt32(int bitcount)
+			{
+				uint ret = 0;
+				for (int i = 0; i < bitcount; i++)
+				{
+					ret = (ret << 1) | ((uint)ReadBit());
+				}
+				return ret;
+			}
+		}
+	}
+}
---
+++ Assets/VR180Mesh.cs
@@ -0,0 +1,200 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Linq;
+using UnityEngine.Video;
+
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshFilter))]
+public class VR180Mesh : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start()
+	{
+		var meshFilter = GetComponent<MeshFilter>();
+		var vp = GetComponent<VideoPlayer>();
+
+		var bytes = GetMshpBin(vp.url);
+		var meshes = MeshProjectionBoxParser.MeshProjectionBox.Parse(bytes);
+
+		if (meshes != null && meshes.Length > 0)
+		{
+			//	VR180 では MeshBox はおそらく 2 個きます (左右で 1 つずつ) がここでは左側決め打ちで。
+			var unityMesh = new Mesh();
+			var mesh = meshes[0];
+
+			{
+				var unityVerticies = new Vector3[mesh.verticies.Length];
+				var unityUVs = new Vector2[mesh.verticies.Length];
+
+				var vertIndex = new Vector3Int();
+				var uvIndex = new Vector2Int();
+				for (int i = 0; i < mesh.verticies.Length; i++)
+				{
+					vertIndex.x += mesh.verticies[i].x_index_delta;
+					vertIndex.y += mesh.verticies[i].y_index_delta;
+					vertIndex.z += mesh.verticies[i].z_index_delta;
+					uvIndex.x += mesh.verticies[i].u_index_delta;
+					uvIndex.y += mesh.verticies[i].v_index_delta;
+
+					//	Mesh Box は右手系なので左手系にする。
+					var vert = new Vector3(mesh.coordinates[vertIndex.x], mesh.coordinates[vertIndex.y], -mesh.coordinates[vertIndex.z]);
+
+					var uv = new Vector2(mesh.coordinates[uvIndex.x], mesh.coordinates[uvIndex.y]);
+					unityVerticies[i] = vert;
+					unityUVs[i] = uv;
+				}
+
+				unityMesh.vertices = unityVerticies;
+				unityMesh.uv = unityUVs;
+			}
+
+			{
+				var indicies = new int[mesh.vertex_lists[0].index_as_delta.Length];
+				int index = 0;
+				for (int i = 0; i < mesh.vertex_lists[0].index_as_delta.Length; i++)
+				{
+					index += mesh.vertex_lists[0].index_as_delta[i];
+					indicies[i] = index;
+				}
+
+				//	Triangle Strip 決め打ち
+				var unityIndicies = new List<int>();
+				for (int i = 0; i < indicies.Length - 2; i++)
+				{
+					if ((i & 1) != 0)
+					{
+						unityIndicies.Add(indicies[i + 1]);
+						unityIndicies.Add(indicies[i + 2]);
+						unityIndicies.Add(indicies[i]);
+					}
+					else
+					{
+						unityIndicies.Add(indicies[i + 2]);
+						unityIndicies.Add(indicies[i + 1]);
+						unityIndicies.Add(indicies[i]);
+					}
+				}
+
+				unityMesh.triangles = unityIndicies.ToArray();
+			}
+			meshFilter.mesh = unityMesh;
+		}
+
+		vp.Play();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	private byte[] GetMshpBin(string fileName)
+	{
+		using (var fs = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+		using (var s = new BufferedStream(fs))
+		{
+			//	途中で Box が見つからないと NullReferenceException
+			//	.NET 4 にして ?. を使う方がよい
+			var box = BoxHeader.GetBoxHeader(s)
+				.Find(s, "moov").Enter(s)
+				.Find(s, "trak").Enter(s)
+				.Find(s, "mdia").Enter(s)
+				.Find(s, "minf").Enter(s)
+				.Find(s, "stbl").Enter(s)
+				.Find(s, "stsd").Enter(s, 8)
+				.Find(s, "avc1").Enter(s, 0x4e)
+				.Find(s, "sv3d").Enter(s)
+				.Find(s, "proj").Enter(s)
+				.Find(s, "mshp");
+			if (box != null)
+			{
+				return box.GetBox(s);
+			}
+
+			return null;
+		}
+	}
+
+
+	private class BoxHeader
+	{
+		public long PositionHead;
+		public long PositionBody;
+		public long Size;
+		public string BoxType;
+
+		public BoxHeader MoveNext(Stream s)
+		{
+			s.Seek(PositionHead + Size, SeekOrigin.Begin);
+			return GetBoxHeader(s);
+		}
+
+		public byte[] GetBox(Stream s)
+		{
+			var ret = new byte[Size];
+
+			s.Seek(PositionHead, SeekOrigin.Begin);
+			s.Read(ret, 0, ret.Length);
+
+			return ret;
+		}
+
+		public BoxHeader Enter(Stream s, int offset = 0)
+		{
+			s.Seek(PositionBody + offset, SeekOrigin.Begin);
+			return GetBoxHeader(s);
+		}
+
+		public BoxHeader Find(Stream s, string boxType)
+		{
+			var box = this;
+			while (box != null && box.BoxType != boxType)
+			{
+				box = box.MoveNext(s);
+			}
+			return box;
+		}
+
+		public static BoxHeader GetBoxHeader(Stream s)
+		{
+			var pos = s.Position;
+			var bsize = new byte[4];
+			var bboxtype = new byte[4];
+			if (s.Read(bsize, 0, bsize.Length) < bsize.Length)
+			{
+				return null;
+			}
+			if (s.Read(bboxtype, 0, bboxtype.Length) < bboxtype.Length)
+			{
+				return null;
+			}
+
+			var ret = new BoxHeader();
+
+			ret.PositionHead = pos;
+			ret.Size = System.BitConverter.ToUInt32(bsize.Reverse().ToArray(), 0);
+			ret.BoxType = System.Text.Encoding.ASCII.GetString(bboxtype);
+
+			if (ret.Size == 1)
+			{
+				var bsize8 = new byte[8];
+				if (s.Read(bsize8, 0, bsize8.Length) < bsize8.Length)
+				{
+					return null;
+				}
+
+				bsize8 = bsize8.Reverse().ToArray();
+				ret.Size = System.BitConverter.ToInt64(bsize8, 0);
+			}
+
+			ret.PositionBody = s.Position;
+
+			return ret;
+		}
+	}
+
+}
